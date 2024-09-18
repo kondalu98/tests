15a1dfe8d6b9a2a37aada01f8a73c5704d7662d8
kondalu98 <pavulurikondalu309@gmail.com>
2024-09-18 12:48:30 +0530

    second

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+import './index.css';
+
+import App from './App';
+import Boot from './Boot';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Boot></Boot>
   </React.StrictMode>
 );
 
